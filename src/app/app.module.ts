79b831fc0414ff117c04c0c66099ca84af000109
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-11 02:13:16 +0200

    Sprint 2 - Ajouter un nouveau produit : ajouter le composant "ajout"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListeProduitsComponent } from './liste-produits/liste-produits.component';
+import { AjoutComponent } from './ajout/ajout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListeProduitsComponent
+    ListeProduitsComponent,
+    AjoutComponent
   ],
   imports: [
     BrowserModule,
