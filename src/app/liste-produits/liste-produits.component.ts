92b03fa88474d6dbf5aa73772876fbd86df80c51
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-11 02:08:50 +0200

    Sprint 1 - Afficher la liste des produits chimiques : alimenter le tableau par défaut

--- src/app/liste-produits/liste-produits.component.ts
+++ src/app/liste-produits/liste-produits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../models/produit';
 
 @Component({
   selector: 'app-liste-produits',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-produits.component.css']
 })
 export class ListeProduitsComponent implements OnInit {
+  produits : Produit[];
 
-  constructor() { }
+  constructor() {
+    this.produits = [];
+  }
 
   ngOnInit(): void {
+    this.produits = [
+      new Produit(1, "n-Butyllithium solution", "109-72-8", "CH3(CH2)3Li", 64.06, "1.6M in hexanes", 3, "LSO-S305"),
+      new Produit(2, "Methanol", "67-56-1", "CH3OH", 32.04, "Methanol, anhydrous, 99.8%", 5, "LSO-S101")
+    ];
   }
 
 }
